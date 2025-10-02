dcc40f2ea6ea3236c239f9ebf27a1bb8bebad10d
sasankgit <sasank4pr@gmail.com>
2025-10-02 20:33:47 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+import Home from './pages/home.jsx'
+
+import './App.css'
+
+function App() {
+
+  return (
+    <div>
+      <Home/>
+    </div>
+
+
+  )
+}
+
+export default App
---
+++ src/pages/home.jsx
@@ -0,0 +1,9 @@
+
+
+export default function  Home(){
+    return(
+    <div> 
+       <h1 className="text-2xl border-amber-50">Hello there</h1>
+    </div>
+    )
+}
