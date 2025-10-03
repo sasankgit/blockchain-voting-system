131a1618c1c0ca4093ae912452e0a2bc63aac1b5
sasankgit <sasank4pr@gmail.com>
2025-10-03 14:26:28 +0530

    navbar added

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -3,7 +3,8 @@
 export default function  Home(){
     return(
     <div> 
-       <h1 className="text-2xl border-amber-50">Hello there</h1>
+       <h1 className="text-2xl border-amber-50 ">Hello there</h1>
+       <p>this is a page for voting using blockchain</p>
     </div>
     )
 }
