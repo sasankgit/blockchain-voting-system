b798ad7387a587ae28084c937cd3b30904ae94a3
sasankgit <sasank4pr@gmail.com>
2025-10-14 00:25:51 +0530

    server code and consts added

--- src/pages/signup.jsx
+++ src/pages/signup.jsx
@@ -1,8 +1,14 @@
 
+import { useState } from "react";
+import {Link} from "react-router-dom";
 
 function Signup(){
+    const [username,setUsername] = useState()
+    const [password,setPassword] = useState()
+
+
     return(
-        <div className="bg-white text-black items-center py-4 px-3">
+        <div className="bg-white text-black items-center py-4 px-3 rounded-2xl ">
             <form>
                <div className="text-lg p-4"> 
                 <label className="text-x1 font-medium text-red-900">USERNAME:</label>
@@ -10,6 +16,7 @@ function Signup(){
                 type = "text"
                 placeholder="enter username"
                 className="text-amber-950 text-medium "
+                onChange={(e) => setUsername(e.target.value)}
                 />
                </div>
                <div className="text-lg p-4">
@@ -17,13 +24,21 @@ function Signup(){
                <input
                type = "password"
                placeholder="enter password"
-               
-               
+               onChange={(e) => setPassword(e.target.value)}
                />
+
                </div>
-               
+               <div className="text-lg p-4">
+               <label className="text-x1 font-medium text-red-900">confirm password</label>
+               <input
+               type = "password"
+               placeholder="enter password"
+               />
+
+               </div>     
                 
             </form>
+            <Link to ="/login" className="text-black ">login page redirect</Link>
         </div>
     )
 }
