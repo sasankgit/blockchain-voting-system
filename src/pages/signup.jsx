a7497402fd150ba5e54af0ea4e053b4dd4d2126e
sasankgit <sasank4pr@gmail.com>
2025-10-15 23:10:06 +0530

    added if else password check

--- src/pages/signup.jsx
+++ src/pages/signup.jsx
@@ -1,16 +1,28 @@
 
 import { useState } from "react";
 import {Link} from "react-router-dom";
+import axios from 'axios'
 
 function Signup(){
     const [username,setUsername] = useState()
+    const [email,setEmail] = useState()
     const [password,setPassword] = useState()
     const [confirmpassword,setConfirmpassword] = useState()
-
+    
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        if(password == confirmpassword){
+          console.log("hello uwu same")
+          axios.post('' , { username,email,password})
+        } 
+        else{
+            console.log("hello uwu different")
+        } 
+    }
 
     return(
         <div className="bg-white text-black items-center py-4 px-3 rounded-2xl ">
-            <form>
+            <form onSubmit={handleSubmit}>
                <div className="text-lg p-4"> 
                 <label className="text-x1 font-medium text-red-900">USERNAME:</label>
                 <input 
@@ -18,6 +30,15 @@ function Signup(){
                 placeholder="enter username"
                 className="text-amber-950 text-medium "
                 onChange={(e) => setUsername(e.target.value)}
+                />
+               </div>
+               <div className="text-lg p-4"> 
+                <label className="text-x1 font-medium text-red-900">Email:</label>
+                <input 
+                type = "text"
+                placeholder="enter username"
+                className="text-amber-950 text-medium "
+                onChange={(e) => SetEmail(e.target.value)}
                 />
                </div>
                <div className="text-lg p-4">
@@ -36,6 +57,10 @@ function Signup(){
                placeholder="enter password"
                onChange={(e) => setConfirmpassword(e.target.value)}
                />
+               </div>
+               <div>
+                <button className="bg-red-800  text-white  px-5 py-2 rounded-2xl"
+                onClick={handleSubmit}>submit</button>
                </div>
 
                   
