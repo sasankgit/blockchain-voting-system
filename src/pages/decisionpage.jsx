69405d9740529deb431a7e3444f604a3dc44f564
sasankgit <sasank4pr@gmail.com>
2025-10-17 12:57:32 +0530

    the signup page is now working

--- src/pages/decisionpage.jsx
+++ src/pages/decisionpage.jsx
@@ -5,10 +5,10 @@ function Redirect(){
     return(
         <div>
             <Link to = "/signup">
-              <button className="bg-amber-900 p-4 fixed left-34 rounded-2xl">signup page</button>
+              <button className="bg-amber-900 p-4 fixed left- rounded-2xl">signup page</button>
             </Link>
             <Link to = "/login">
-              <button className="bg-amber-900 p-4 fixed left-100 rounded-2xl" >Login page</button>
+              <button className="bg-amber-900 p-4 fixed left-160 rounded-2xl" >Login page</button>
             </Link>
         </div>
     );
